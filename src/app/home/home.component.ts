48973d584951a1874a8a426f807f8ee30677f459
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-11-06 20:02:37 +0530

    Using more than one operators in observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, interval, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -34,7 +34,9 @@ export class HomeComponent implements OnInit, OnDestroy {
     //   return `Round : ${data+1}`
     // }))
 
-    this.observableId = customIntervalObservable.pipe(map((data: number) => {
+    this.observableId = customIntervalObservable.pipe(filter((data:number)=>{
+      return data>0;
+    }),map((data: number) => {
       return `Round : ${data + 1}`
     })).subscribe((data) => {
       console.log(data)
